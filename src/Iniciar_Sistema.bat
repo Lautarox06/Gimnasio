f343b9fb865a62e6649effbfa940b5400da55f4c
Lautarox06 <83470554+Lautarox06@users.noreply.github.com>
2025-11-30 16:32:46 -0300

    .

---
+++ src/Iniciar_Sistema.bat
@@ -0,0 +1,14 @@
+@echo off
+title SISTEMA GESTION GIMNASIO - GRUPO 4
+
+color 0A
+
+:: Limpia la pantalla
+cls
+
+echo ==============================================
+
+java -jar Gimnasio.jar
+
+echo ==============================================
+pause
